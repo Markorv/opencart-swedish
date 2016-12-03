df77aa4612bdd1f9a37616d00b66c24a7d370de6
Alexander Ohlén <alexander@waixan.se>
2016-12-03 01:47:27 +0100

    Removed abusive translation of powered by phrase

--- catalog/language/sv-se/common/footer.php
+++ catalog/language/sv-se/common/footer.php
@@ -14,5 +14,5 @@ $_['text_account']      = 'Mitt konto';
 $_['text_order']        = 'Orderhistorik';
 $_['text_wishlist']     = 'Önskelista';
 $_['text_newsletter']   = 'Nyhetsbrev';
-$_['text_powered']      = 'Maxhost.se - En komplett lösning för din e-butik';
+$_['text_powered']      = 'Butiken drivs med hjälp av <a href="http://www.opencart.com">OpenCart</a><br /> %s &copy; %s';
 ?>
